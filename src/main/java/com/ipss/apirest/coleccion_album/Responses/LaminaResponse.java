e08218d953827c5c60649b423dc99a50c31efe2a
Mario Quevedo <mbquevedodev@gmail.com>
2024-12-28 04:05:18 -0300

    refactor: Update LaminaResponse to use a generic type for data parameter

--- src/main/java/com/ipss/apirest/coleccion_album/Responses/LaminaResponse.java
+++ src/main/java/com/ipss/apirest/coleccion_album/Responses/LaminaResponse.java
@@ -1,7 +1,6 @@
 package com.ipss.apirest.coleccion_album.Responses;
 
 import java.util.List;
-
 import com.ipss.apirest.coleccion_album.Models.Lamina;
 
 import lombok.Data;
@@ -9,13 +8,13 @@ import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class LaminaResponse extends ApiResponse<List<Lamina>> {
-  public LaminaResponse(int status, String message, List<Lamina> data) {
+public class LaminaResponse extends ApiResponse<Object> {
+
+  public <T> LaminaResponse(int status, String message, List<T> data) {
     super(status, message, data);
   }
 
   public LaminaResponse(int status, String message, Lamina lamina) {
     super(status, message, List.of(lamina));
   }
-
 }
